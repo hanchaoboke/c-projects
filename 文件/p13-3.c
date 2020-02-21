b519094cbc974e076ae21b14ca8c58af19912fe3
hanchao <1035710787@qq.com>
2020-02-21 11:50:20 +0800

    文件

--- 文件/p13-3.c
+++ 文件/p13-3.c
@@ -1,9 +1,10 @@
 /*
 练习 13-3
 改写代码清单13-2中的程序，将从文件读入的个人信息按身高排序后显示。
-还未完成
+
 */
 #include<stdio.h>
+#include<string.h>
 int main(int argc, char const *argv[])
 {
     FILE *fp;
@@ -12,6 +13,15 @@ int main(int argc, char const *argv[])
     double height,weight;
     double hsum = 0;
     double wsum = 0;
+    typedef struct person
+    {
+        char name[100];
+        double height;
+        double weight;
+    }Person;
+    Person person[100];
+
+    int i = 1;
     
 
     if ((fp = fopen("hw.dat","r")) == NULL)
@@ -21,11 +31,36 @@ int main(int argc, char const *argv[])
     {
         while (fscanf(fp, "%s%lf%lf",name,&height,&weight) == 3)
         {
-
-            printf("%-10s %5.1f %5.1f\n",name, height, weight);
+            strcpy(person[i].name, name);
+            person[i].height = height;
+            person[i].weight = weight;
+            i++;
             ninzu++;
             hsum += height;
             wsum += weight;
+        }
+        for (int j = 0; j < i; j++)
+        {
+            for (int z = 1; z <= i; z++)
+            {
+                Person p;
+                if (person[z].height > person[z+1].height)
+                {
+                    p = person[z];
+                    person[z] = person[z+1];
+                    person[z+1] = p;
+                }
+                
+            }
+            
+        }
+        
+
+
+
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%-10s %5.1f %5.1f\n",person[i].name, person[i].height, person[i].weight);
         }
         printf("------------------------------\n");
         printf("平均       %5.1f %5.1f\n",hsum / ninzu, wsum / ninzu);
